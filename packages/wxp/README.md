e9dc078aff029b0f783999f74147d2c8ea9ccb8c
wqdygkd <cui3155@gmail.com>
2025-05-22 18:54:18 +0800

    更新

---
+++ packages/wxp/README.md
@@ -0,0 +1,2 @@
+let n = 'http://' + e + ':2493/' + '/wxpresource/' + process.platform
+https://github.com/create-dmg/create-dmg
---
+++ packages/wxp/create.sh
@@ -0,0 +1,11 @@
+create-dmg \
+  --volname "WxpClient" \
+  --volicon "WxP Client v3.1.23.app/Contents/Resources/icon.icns" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "WxP Client v3.1.23.app" 200 190 \
+  --hide-extension "WxP Client v3.1.23.app" \
+  --app-drop-link 600 185 \
+  "WxpClient.dmg" \
+  "source_folder/"
